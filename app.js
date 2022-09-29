167adb33cccc867db68f63e6b3c9de92acbdbeff
Mariya Setko <mariya.setko@gmail.com>
2022-09-29 15:00:16 -0400

    keyCode control function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const square = document.querySelectorAll('.grid div')
-    cosnt scoreDisplay = document.querySelector('span')
+    const scoreDisplay = document.querySelector('span')
     const startBtn = document.querySelector('.start')
 
     const width = 10
@@ -13,5 +13,23 @@ document.addEventListener('DOMContentLoaded', () => {
     let intervalTime = 0
     let interval = 0
 
+    //start game
+
     //assign functions to keycodes
+    function control(e) {
+        squares[currentIndex].classList.remove('snake') //removing class of snake from squares
+
+        if(e.keyCode === 39) {
+            direction = 1 //RIGHT
+        } else if (e.keyCode === 38) {
+            direction = -width //UP
+        } else if (e.keyCode === 37) {
+            direction = -1 //LEFT
+        } else if (e.keyCode === 40) {
+            direction = +width //DOWN
+        }   
+    }
+
+document.addEventListener('keyup', control)
+
 })
