82aaf6af5a12038519b6f6716d58519c0ee65f2a
Mariya Setko <mariya.setko@gmail.com>
2022-10-01 13:43:08 -0400

    finished

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
         squares[appleIndex].classList.remove('apple')
         clearInterval(interval)
         score = 0
-        //randomApple()
+        randomApple()
         direction = 1
         scoreDisplay.innerText = score
         intervalTime = 1000
@@ -54,7 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
         squares[currentSnake[0]].classList.remove('apple')
         squares[tail].classList.add('snake')
         currentSnake.push(tail)
-        // randomApple()
+        randomApple()
         score++
         scoreDisplay.textContent = score
         clearInterval(interval)
